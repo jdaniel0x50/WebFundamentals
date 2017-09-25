b01d5b6086f981d039721fafead79452b74215ac
Joshua Daniel <joshuadaniel@fuller.edu>
2017-09-25 17:01:38 -0500

    completed JS assignment numbers only array

---
+++ JavaScript/JS_8NumbersOnlyArray.js
@@ -0,0 +1,28 @@
+function numbersOnly(arr) {
+    var newarr = [];
+    for (var i=0; i<arr.length; i++) {
+        if (typeof arr[i] === "number") {
+            newarr.push(arr[i]);
+        }
+    }
+    return newarr;
+}
+
+function overwriteArray(arr) {
+    for (var i=0; i<arr.length; i++) {
+        if (typeof arr[i] === "number") {
+            for (var j=i; j<arr.length; j++) {
+                arr[j] = arr[j+1];
+            }
+            arr.pop();
+            i--;
+        }
+    }
+    return arr;
+}
+
+
+
+var arr = [1, "apple", -3, "orange", 0.5, 99.99, "coding", -100, "dojo", -0.5, "hello"];
+console.log(numbersOnly(arr));
+console.log(overwriteArray(arr));
