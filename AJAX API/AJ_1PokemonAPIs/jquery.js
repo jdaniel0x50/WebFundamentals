647f6bd7fde7b5b11cfce8a3b6009a23dd11802b
Joshua Daniel <joshuadaniel@fuller.edu>
2017-09-27 13:43:21 -0500

    completed first AJAX assignment pokemon

---
+++ AJAX API/AJ_1PokemonAPIs/jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    //$('body').append("<img src=")
+    for (var i=0; i<152; i++) {
+        $('body').append("<img class='charimg' src='http://pokeapi.co/media/img/" + i + ".png'>")
+    }
+});
